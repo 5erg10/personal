d6535d97994b6f81331c200ad5ab8428e7216a31
5erg1kal <santamaria.fajardo@gmail.com>
2024-11-30 11:16:48 +0100

    add scene ilumination and shadowmap

--- js/scene.js
+++ js/scene.js
@@ -37,14 +37,14 @@ export const initRender = async () => {
 
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = true;
 	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 	container.appendChild( renderer.domElement );
 
-	scene.background = new THREE.Color( 0x080c24);
-
-	const light = new THREE.AmbientLight( 0xaaaaaa ); // soft white light
-	scene.add( light );
+	scene.background = new THREE.Color( 0x00040d);
 
 	camera.position.set( 0, 1, 4 );
 
@@ -82,6 +82,7 @@ export const initRender = async () => {
 	scene.add( room );
 
 	$('.loading').toggleClass('closed');
+
 	animate();
 
 	window.onresize = function () {
@@ -123,8 +124,12 @@ export const initRender = async () => {
 					if (obj.isMesh && obj.material.map) {
 						obj.material.map.encoding = THREE.sRGBEncoding;
 					}
-				})
-				return resolve(homeOfficeModel)
+				});
+				
+				const light = new THREE.AmbientLight( 0xaaaaaa ); // soft white light
+				scene.add( light );
+
+				return resolve(homeOfficeModel);
 
 			}, function ( xhr ) {
 				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 253596696) * 100)}%`;
@@ -142,12 +147,18 @@ export const initRender = async () => {
 				apocalyptoRoomModel.rotation.set( 0, 0, 0 );
 				apocalyptoRoomModel.scale.set( 1, 1, 1 );
 				rotationValues = { x: 0, y: 0, z: 0 };
+
 				apocalyptoRoomModel?.children?.forEach( obj => {
 					if (obj.isMesh && obj.material.map) {
 						obj.material.map.encoding = THREE.sRGBEncoding;
 					}
-				})
-				return resolve(apocalyptoRoomModel)
+				});
+
+				const light = new THREE.AmbientLight( 0xaaaaaa ); // soft white light
+				scene.add( light );
+
+				return resolve(apocalyptoRoomModel);
+
 			}, function ( xhr ) {
 				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 62890612) * 100)}%`;
 			}, function ( e ) {
@@ -164,12 +175,11 @@ export const initRender = async () => {
 				comicRoomModel.rotation.set( 0, 15, 0 );
 				comicRoomModel.scale.set( 1, 1, 1 );
 				rotationValues = { x: 0, y: 15, z: 0 };
-				comicRoomModel?.children?.forEach( obj => {
-					if (obj.isMesh && obj.material.map) {
-						obj.material.map.encoding = THREE.sRGBEncoding;
-					}
-				})
-				return resolve(comicRoomModel)
+
+				const light = new THREE.AmbientLight( 0x777777 ); // soft white light
+				scene.add( light );
+
+				return resolve(comicRoomModel);
 
 			}, function ( xhr ) {
 				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 33758656) * 100)}%`;
@@ -182,20 +192,43 @@ export const initRender = async () => {
 	function ninetysOffice(loader) {
 		return new Promise((resolve, reject) => {
 			loader.load( 'models/90s_office.glb', function ( gltf ) {
-				const comicRoomModel = gltf.scene;
-				comicRoomModel.position.set( 0, -0.3, 2 );
-				comicRoomModel.rotation.set( 0, 0, 0 );
-				comicRoomModel.scale.set( 1, 1, 1 );
+
+				const ninetysOfficeModel = gltf.scene;
+				ninetysOfficeModel.position.set( 0, -0.3, 2 );
+				ninetysOfficeModel.rotation.set( 0, 0, 0 );
+				ninetysOfficeModel.scale.set( 1, 1, 1 );
+				ninetysOfficeModel.castShadow = true;
+				ninetysOfficeModel.receiveShadow = true;
+				ninetysOfficeModel.traverse( function ( node ) {
+                    if ( node.isMesh || node.isLight ) node.castShadow = true;
+                    if ( node.isMesh || node.isLight ) node.receiveShadow = true;
+                } );
+
 				rotationValues = { x: 0.5, y: -7.5, z: 0 };
-				comicRoomModel?.children?.forEach( obj => {
-					if (obj.isMesh && obj.material.map) {
-						obj.material.map.encoding = THREE.sRGBEncoding;
-					}
-				})
-				return resolve(comicRoomModel)
+
+				const lightTargetObject = new THREE.Object3D();
+				lightTargetObject.position.set(-0.45, 0, -0.9);
+				ninetysOfficeModel.add(lightTargetObject);
+
+				const spotLight = new THREE.SpotLight( 0xffffaa, 1 );
+				spotLight.position.set( -0.45, 1.1, -0.9 );
+				spotLight.shadow.bias = -0.001;
+				spotLight.castShadow = true;
+				spotLight.shadow.camera.near = 0.1;
+				spotLight.target = lightTargetObject;
+
+				// const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+				// ninetysOfficeModel.add( spotLightHelper );
+				
+				ninetysOfficeModel.add( spotLight );
+
+				const light = new THREE.AmbientLight( 0x444444 ); // soft white light
+				scene.add( light );
+
+				return resolve(ninetysOfficeModel);
 
 			}, function ( xhr ) {
-				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 33758656) * 100)}%`;
+				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 5151624) * 100)}%`;
 			}, function ( e ) {
 				console.error( e );
 			});
@@ -204,21 +237,40 @@ export const initRender = async () => {
 
 	function ninetysOfficeCubicle(loader) {
 		return new Promise((resolve, reject) => {
+			
 			loader.load( 'models/90s_office_cubicle.glb', function ( gltf ) {
-				const comicRoomModel = gltf.scene;
-				comicRoomModel.position.set( 0, -4, -6 );
-				comicRoomModel.rotation.set( 0.3, -8.5, 0 );
-				comicRoomModel.scale.set( 1, 1, 1 );
+				
+				const ninetysOfficeCubicleModel = gltf.scene;
+				ninetysOfficeCubicleModel.position.set( 0, -4, -6 );
+				ninetysOfficeCubicleModel.rotation.set( 0.3, -8.5, 0 );
+				ninetysOfficeCubicleModel.scale.set( 1, 1, 1 );
+				ninetysOfficeCubicleModel.traverse( function ( node ) {
+                    if ( node.isMesh || node.isLight ) node.castShadow = true;
+                    if ( node.isMesh || node.isLight ) node.receiveShadow = true;
+                } );
+
 				rotationValues = { x: 0.3, y: -8.5, z: 0 };
-				comicRoomModel?.children?.forEach( obj => {
-					if (obj.isMesh && obj.material.map) {
-						obj.material.map.encoding = THREE.sRGBEncoding;
-					}
-				})
-				return resolve(comicRoomModel)
+
+				const lightTargetObject = new THREE.Object3D();
+				lightTargetObject.position.set(-3.2, 4, 1.3);
+				ninetysOfficeCubicleModel.add(lightTargetObject);
+
+				const spotLight = new THREE.SpotLight( 0xffffaa, 5 );
+				spotLight.position.set( -3.2, 6, 1.3 );
+				spotLight.castShadow = true;
+				spotLight.shadow.bias = -0.001;
+
+				spotLight.target = lightTargetObject;
+				
+				ninetysOfficeCubicleModel.add( spotLight );
+
+				const light = new THREE.AmbientLight( 0x444444 ); // soft white light
+				ninetysOfficeCubicleModel.add( light );
+				
+				return resolve(ninetysOfficeCubicleModel)
 
 			}, function ( xhr ) {
-				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 33758656) * 100)}%`;
+				$('.loading')[0].innerText = `Loading ${Math.round((xhr.loaded / 28025508) * 100)}%`;
 			}, function ( e ) {
 				console.error( e );
 			});
