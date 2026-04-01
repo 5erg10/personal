86f479f65e464ce79512dc1eb595801ad2097b8e
5erg1kal <santamaria.fajardo@gmail.com>
2026-04-01 08:46:06 +0200

    remove unnecesary files

--- data/cred.js
+++
@@ -1,9 +0,0 @@
-const process = {
-    env: {
-        API_KEY: 'Bearer KZMEIIKAUGBZ3CFHHYF2ZPITQOJQESJ2',
-        API_URL: 'https://api.wit.ai/message?v=20240304&q=',
-        EMAIL_JS_USER: 'wmOWwFMP1VytTUw29'
-    }
-}
-
-export default process
